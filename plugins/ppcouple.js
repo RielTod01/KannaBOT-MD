d04ecbe57a2319230cf65936b244eca295ea8cc6
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-05-30 01:19:23 +0700

    Create ppcouple.js

---
+++ plugins/ppcouple.js
@@ -0,0 +1,15 @@
+let fetch = require("node-fetch")
+
+let handler = async (m, { conn }) => {
+  let res = await fetch(global.API('xteam', '/randomimage/ppcouple', {}, 'APIKEY=f04c164fdec6c033'))
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  if (!json.status) throw json
+  await conn.sendFile(m.chat, json.result.male, '', 'cowo', m)
+  await conn.sendFile(m.chat, json.result.female, '', 'cewe', m)
+}
+handler.help = ['ppcouple', 'ppcp']
+handler.tags = ['anime']
+handler.command = /^(pp(cp|couple))$/i
+
+module.exports = handler
