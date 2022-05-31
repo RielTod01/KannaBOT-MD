efde5cb754b1ec397c6d80cbfeb7d51303e711ee
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-05-31 12:47:37 +0700

    Update anime-neko.js

--- plugins/anime-neko.js
+++ plugins/anime-neko.js
@@ -1,15 +1,11 @@
 import fetch from 'node-fetch'
 
-let handler = async (m, { conn }) => {
-	let ne = await (await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/neko.txt')).text()
-    let nek = ne.split('\n')
-    let neko = pickRandom(nek)
-	conn.sendButton(m.chat, 'Nyaww~ 🐾💗', wm, neko, [['Next','.neko']],m)
+let handler = async (m, { conn, command }) => {
+	let url = 'https://api.lolhuman.xyz/api/random/neko?apikey=SGWN'
+	conn.sendButton(m.chat, 'Nyaww~ 🐾💗', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
 }
 handler.command = /^(neko)$/i
 handler.tags = ['anime']
 handler.help = ['neko']
+
 export default handler
-function pickRandom(list) {
-  return list[Math.floor(Math.random() * list.length)]
-}
