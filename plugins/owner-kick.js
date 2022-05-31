485282e7472c3003c1ba076e6634e15fe529df65
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-05-31 08:58:39 +0700

    Update owner-kick.js

--- plugins/owner-kick.js
+++ plugins/owner-kick.js
@@ -15,7 +15,7 @@ handler.help = ['kick', '-'].map(v => 'o' + v + ' @user')
 handler.tags = ['owner']
 handler.command = /^(okick|o-)$/i
 
-handler.owner = true
+handler.Admin = true
 handler.group = true
 handler.botAdmin = true
 
