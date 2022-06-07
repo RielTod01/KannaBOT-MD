818c22c41d60f86f33ca1f9277afe3984a9a757f
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-06-08 01:16:11 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,17 +1,26 @@
-import { tiktokdl, tiktokdlv3 } from '@bochilteam/scraper'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    const { author: { nickname }, video, description } = await tiktokdl(args[0]).catch(async _ => await tiktokdlv2(args[0]))
-    const url = video.no_watermark_raw || video.no_watermark || video.no_watermark_hd || video.with_watermark 
-    if (!url) throw 'Can\'t download video!'
-    conn.sendFile(m.chat, url, 'tiktok.mp4', `
-🔗 *Url:* ${url}
-🧏 *Nickname:* ${nickname}${description ? `🖹 *Description:* ${description}` : ''}
-`.trim(), m)
+import xa from 'xfarr-api'
+let handler = async(m, { conn, usedPrefix, args, command }) => {
+    if(!args[0]) throw `Harap masukkan URL sebagai parameter!\n\nContoh: ${usedPrefix + command} https://vt.tiktok.com/ZSdNmfmeJ/`
+    xa.Tiktok(args[0])
+    .then(async data => {   
+    let caption = `                   *「 T I K T O K 」*
+                 ████████▀▀▀████
+                 ████████────▀██
+                 ████████──█▄──█
+                 ███▀▀▀██──█████
+                 █▀──▄▄██──█████
+                 █──█████──█████
+                 █▄──▀▀▀──▄█████
+                 ███▄▄▄▄▄███████
+──────────────────────────
+*📛 Title:* ${data.title}
+*🖼️ Quality:* ${data.medias[1].quality}
+*📊 Size:* ${data.medias[1].forrmattedSize}`
+    await conn.sendHydrated(m.chat, caption, global.wm, data.medias[1].url, `${args[0]}`, '🌎 L I N K', null, null, [[null, null],[null,null],[null,null]], m) 
+    })
 }
-handler.help = ['tiktok'].map(v => v + ' <url>')
+handler.command = /^(tiktok|tk|tkdl|td)$/i
 handler.tags = ['downloader']
-
-handler.command = /^(tik(tok)?(dl)?)$/i
+handler.help = ['tiktok <url>']
 
 export default handler
