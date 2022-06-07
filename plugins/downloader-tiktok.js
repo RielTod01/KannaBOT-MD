d9141c872992014193192a30baeb746dbf01055a
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-06-08 00:48:52 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,4 +1,4 @@
-import { tiktokdl, tiktokdlv2 } from '@bochilteam/scraper'
+import { tiktokdl, tiktokdlv3 } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
     const { author: { nickname }, video, description } = await tiktokdl(args[0]).catch(async _ => await tiktokdlv2(args[0]))
