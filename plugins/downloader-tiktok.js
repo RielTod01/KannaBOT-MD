c93ea4434bceb96f9298c9c6812c82052f1e6966
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-06-10 19:00:27 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -2,15 +2,18 @@ import fetch from 'node-fetch'
 let handler = async (m { conn, args, usedPrefix, command }) => {
   if (!args[0]) throw 'Uhm...url nya mana?'
 let res = await fetch('https://api.lolhuman.xyz/api/tiktok?apikey=SGWN&url=${args[0]}'))
-    let {result} = await res.json()
+        let json = await res.json()
+  if (!json.status) throw json
+conn.sendFile(m.chat, json.result.link, wm, m)
 
+}, 
       conn.reply(m.chat, `Downloading media from Tiktok`, 0, {
   contextInfo: { mentionedJid: [m.sender],
     externalAdReply :{
     mediaUrl: 'https://vt.tiktok.com' ,
     mediaType: 2,
     description: wm , 
-    title: 'Tiktok' ,
+    title: '               「🇹」 ᴛ ɪ ᴋ ᴛ ᴏ ᴋ',
     body: wm, //`${fileSizeH}`,
     thumbnail: await(await fetch(thumb)).buffer(),
     sourceUrl: sgc
