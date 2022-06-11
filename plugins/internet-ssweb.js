14895a140a094097b01ff8b89f889ae07b3eeb1e
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-06-11 13:46:40 +0700

    Update internet-ssweb.js

--- plugins/internet-ssweb.js
+++ plugins/internet-ssweb.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
   import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command, args }) => {
   
-  let res = await(await fetch(`https://api.lolhuman.xyz/api/sswebfull?apikey=SGWN&url=${args[0]}`)).buffer()
+  let res = await(await fetch(`https://api.lolhuman.xyz/api/ssweb?apikey=SGWN&url=${args[0]}`)).buffer()
   if(!args[0]) throw `Linknya mana?`
 conn.sendHydrated(m.chat, `*「 Screenshot Web 」*\n\n*💻 Url:* ${args[0]}`, null, res, `${args[0]}`, '🌎 U R L', null, null, [[null,null],[null,null],[null,null]], m)
 }
