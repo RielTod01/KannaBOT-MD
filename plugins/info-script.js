e20205243a8081df5605229670080ad35ddd7591
RielTod01 <107606413+RielTod01@users.noreply.github.com>
2022-06-18 13:24:53 +0800

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 	// ‼️ NOTE: Link script Jangan diubah, dihapus atau diganti
 let text = `❏ *📮 Script Multi Device*
 │• *Script :* 
-│↳ Private🚫
+│↳ Private deks⛔
 │• *Base :* 
-│↳ github.com/bochilgaming/games-wabot-md/
+│↳ github.com/bochilgaming/games-wabot-md
 ┗──────────═┅═──────────
 
 ❏ *📮 Script Non MD*
@@ -17,15 +17,15 @@ let text = `❏ *📮 Script Multi Device*
 📍 *N o t e :* 
 • Jangan lupa minta izin owner sebelum menggunakan scriptnya kak!
 • Jangan Lupa kasih star, follow & kasih credit
-• Dilarang menjual Script Ini!
+• Dilarang menjual Script Ini ketahuan jual langgsung hangus hapus!
 • Jika menemukan bug di script, harap lapor owner
 • Dilarang reupload sc, Hanya boleh fork
 `
 const templateButtons = [
-    {index: 1, urlButton: {displayText: 'My Github', url: sgh}},
-    {index: 2, urlButton: {displayText: 'Group Official', url: sgc}},
-    {index: 3, quickReplyButton: {displayText: 'Owner', id: '.owner'}},
-    {index: 4, quickReplyButton: {displayText: 'Donasi', id: '.donasi'}},
+    {index: 1, urlButton: {displayText: '🐤𝐆𝐈𝐓𝐇𝐔𝐁 𝐀𝐈𝐍𝐊:𝐕🐤', url: sgh}},
+    {index: 2, urlButton: {displayText: '🤖𝐆𝐑𝐎𝐔𝐏 𝐁𝐎𝐓 𝐑𝐈𝐄𝐋🤖', url: sgc}},
+    {index: 3, quickReplyButton: {displayText: '🎮𝐂𝐑𝐄𝐀𝐓𝐎𝐑 𝐁𝐎𝐓🎮', id: '.owner'}},
+    {index: 4, quickReplyButton: {displayText: '𝐃𝐎𝐍𝐀𝐒𝐈 𝐂𝐎𝐊🗿', id: '.donasi'}},
 ]
 let tm = {
 text: text,
